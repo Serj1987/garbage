bf4f6dc31bf22ce58d22a3fdc204e30c0ece9ab5
Sergey Tarasov <serj-mlp@mail.ru>
2021-12-12 20:02:14 +0300

    new version without screens
    only tabs

--- tabbed_win.py
+++ tabbed_win.py
@@ -1,56 +1,54 @@
-from kivy.uix.anchorlayout import AnchorLayout
-from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager, Screen  # , WipeTransition
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.app import App
 from kivy.core.window import Window
 from kivymd.uix.datatables import MDDataTable
-from kivy.metrics import dp
-import sqlite3
-from kivy.uix.tabbedpanel import TabbedPanel
 
 Window.size = 375, 667
 
 
-class MainMenu(Screen):
+class Main(TabbedPanel):
     pass
 
 
-class SecondScreen(Screen):
-
-    def btn_add_press(self):  # this func work with text from textinput and spinner
-        if self.ids.number_detail.text != '':
-            print(self.ids.number_detail.text, self.ids.name_detail.text,
-                  self.ids.quantity.text, self.ids.comment.text)
-
-            self.ids.number_detail.text = ''
-            self.ids.name_detail.text = 'Наименование'
-            self.ids.quantity.text = ''
-            self.ids.comment.text = ''
-
-class Tab(Screen, TabbedPanel):
-	pass
-
-class Browse(Screen): #, TabbedPanel): # it don't work screen with tabbedpanel
-
-	pass
-
-class TableWindow(Screen):
+class TableWindow():
     def __init__(self, **kw):
         super().__init__(**kw)
         self.data_tables = None
         self.cur = None
         self.rows = None
 
+    def add_table(self):
+        self.con = sqlite3.connect('container.db')
+        self.cur = self.con.cursor()
+        self.cur.execute("SELECT * FROM details")
+        self.rows = self.cur.fetchall()
 
-class TabbedApp(MDApp):
+        layout = AnchorLayout()
+        self.data_tables = MDDataTable(
+            use_pagination=True,
+            column_data=[
+                ("№ детали", dp(20)),
+                ("Наименование", dp(30)),
+                ("Количество", dp(15)),
+                ("Дата", dp(20)),
+                ("Примечание", dp(20)),
+                ("Метка", dp(30)),
+            ],
+            row_data=[self.row for self.row in self.rows],
+        )
+        self.add_widget(self.data_tables)
+        return layout
 
+    def on_enter(self):
+        self.add_table()
+
+
+
+class TabbedApp(App):
     def build(self):
-        sm = ScreenManager()  # transition=WipeTransition())
-        sm.add_widget(MainMenu(name='main_menu'))
-        sm.add_widget(SecondScreen(name='second_screen'))
-        sm.add_widget(Browse(name='browse'))
-#        sm.add_widget(Tab(name='tab'))
-        return sm
+        return Main()
 
 
 if __name__ == '__main__':
     TabbedApp().run()
+
